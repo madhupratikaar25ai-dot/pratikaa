27f4d7ee90629f61c5d7814443a9bb344f13505d
madhu pratika <madhupratikaar25ai@vetias.ac.in>
2026-06-08 14:59:39 +0530

    completed pass and fail

--- src/day3.java
+++ src/day3.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+void main() {
+    /*
+    int n=5;
+    //row loop
+    for(int row=1;row<=n;row++){
+        //column loop
+        for(int column=1;column<=row;column++) {
+            System.out.print(column);
+        }
+        System.out.println();
+        }
 
-public class day3 {
+     */
+
+
+    int[] arr = {87, 32, 44, 89, 32};
+    System.out.println(arr[0]);
+    System.out.print("PAss mark:");
+    for (int i = 0; i <= 4; i++) {
+      int val = arr[i];
+      if (val >= 40) {
+          System.out.print(val+ ", ");
+      }
+    }
+    System.out.print("\nFail mark: ");
+    for (int i = 0; i <= 4; i++) {
+        int val = arr[i];
+        if (val < 40) {
+            System.out.print(val +", " );
+        }
+    }
 }
+
